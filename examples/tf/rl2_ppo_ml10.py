463428c4be2247511a52efe430ba665be727ace4
tsan <ahtsans@gmail.com>
2020-03-20 10:50:30 -0700

    Fix examples

--- examples/tf/rl2_ppo_ml10.py
+++ examples/tf/rl2_ppo_ml10.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-"""Example script to run RL2 in HalfCheetah."""
+"""Example script to run RL2 in ML10."""
+from metaworld.benchmarks import ML10
+
 from garage import wrap_experiment
-from garage.envs.half_cheetah_vel_env import HalfCheetahVelEnv
 from garage.experiment import task_sampler
 from garage.experiment.deterministic import set_seed
 from garage.np.baselines import LinearFeatureBaseline
 from garage.sampler import LocalSampler
-from garage.tf.algos import RL2TRPO
+from garage.tf.algos import RL2PPO
 from garage.tf.algos.rl2 import RL2Env
 from garage.tf.algos.rl2 import RL2Worker
 from garage.tf.experiment import LocalTFRunner
-from garage.tf.optimizers import ConjugateGradientOptimizer
-from garage.tf.optimizers import FiniteDifferenceHvp
 from garage.tf.policies import GaussianGRUPolicy
 
 
 @wrap_experiment
-def rl2_trpo_halfcheetah(ctxt=None,
-                         seed=1,
-                         max_path_length=100,
-                         meta_batch_size=10,
-                         n_epochs=10,
-                         episode_per_task=4):
-    """Train TRPO with HalfCheetah environment.
+def rl2_ppo_ml10(ctxt=None,
+                 seed=1,
+                 max_path_length=150,
+                 meta_batch_size=40,
+                 n_epochs=10,
+                 episode_per_task=10):
+    """Train PPO with ML10 environment.
 
     Args:
         ctxt (garage.experiment.ExperimentContext): The experiment
@@ -37,10 +36,14 @@ def rl2_trpo_halfcheetah(ctxt=None,
     """
     set_seed(seed)
     with LocalTFRunner(snapshot_config=ctxt) as runner:
-        tasks = task_sampler.SetTaskSampler(lambda: RL2Env(
-            env=HalfCheetahVelEnv()))
+        ML_train_envs = [
+            RL2Env(ML10.from_task(task_name))
+            for task_name in ML10.get_train_tasks().all_task_names
+        ]
+        tasks = task_sampler.EnvPoolSampler(ML_train_envs)
+        tasks.grow_pool(meta_batch_size)
 
-        env_spec = RL2Env(env=HalfCheetahVelEnv()).spec
+        env_spec = ML_train_envs[0].spec
         policy = GaussianGRUPolicy(name='policy',
                                    hidden_dim=64,
                                    env_spec=env_spec,
@@ -48,18 +51,25 @@ def rl2_trpo_halfcheetah(ctxt=None,
 
         baseline = LinearFeatureBaseline(env_spec=env_spec)
 
-        algo = RL2TRPO(rl2_max_path_length=max_path_length,
-                       meta_batch_size=meta_batch_size,
-                       task_sampler=tasks,
-                       env_spec=env_spec,
-                       policy=policy,
-                       baseline=baseline,
-                       max_path_length=max_path_length * episode_per_task,
-                       discount=0.99,
-                       max_kl_step=0.01,
-                       optimizer=ConjugateGradientOptimizer,
-                       optimizer_args=dict(hvp_approach=FiniteDifferenceHvp(
-                           base_eps=1e-5)))
+        algo = RL2PPO(rl2_max_path_length=max_path_length,
+                      meta_batch_size=meta_batch_size,
+                      task_sampler=tasks,
+                      env_spec=env_spec,
+                      policy=policy,
+                      baseline=baseline,
+                      discount=0.99,
+                      gae_lambda=0.95,
+                      lr_clip_range=0.2,
+                      pg_loss='surrogate_clip',
+                      optimizer_args=dict(
+                          batch_size=32,
+                          max_epochs=10,
+                      ),
+                      stop_entropy_gradient=True,
+                      entropy_method='max',
+                      policy_ent_coeff=0.02,
+                      center_adv=False,
+                      max_path_length=max_path_length * episode_per_task)
 
         runner.setup(algo,
                      tasks.sample(meta_batch_size),
@@ -72,4 +82,4 @@ def rl2_trpo_halfcheetah(ctxt=None,
                      meta_batch_size)
 
 
-rl2_trpo_halfcheetah(seed=1)
+rl2_ppo_ml10(seed=1)
